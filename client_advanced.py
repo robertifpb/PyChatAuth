35948872f64ef8b592640be68dff6b1aacb6fa61
Álex Robert <alex.braz@academico.ifpb.edu.br>
2025-06-13 11:06:47 -0300

    Alterações finais

--- client_advanced.py
+++ client_advanced.py
@@ -6,49 +6,62 @@ def receber_mensagens(socket_client):
     while True:
         try:
             mensagem = socket_client.recv(1024).decode('utf-8')
+            if not mensagem:
+                print('\nConexão encerrada pelo servidor.')
+                break
             print("\n" + mensagem + "\nDigite mensagem: ", end="")
         except:
             print('\nConexão com o servidor perdida')
-            time.sleep(1)
             break
 
 def iniciar_cliente():
-        HOST = '127.0.0.1'
-        PORTA = 8381
+    HOST = '127.0.0.1'
+    PORTA = 2323
 
-        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-            s.connect((HOST,PORTA))
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        try:
+            s.connect((HOST, PORTA))
 
-            #Autenticação
-            print(s.recv(1024).decode('utf-8'), end="")
-            usuario =  input()
+            # Autenticação
+            print(s.recv(1024).decode('utf-8'), end="")  # Solicita usuário
+            usuario = input()
             s.sendall(usuario.encode('utf-8'))
 
-            print(s.recv(1024).decode('utf-8'), end="")
+            print(s.recv(1024).decode('utf-8'), end="")  # Solicita senha
             senha = input()
             s.sendall(senha.encode('utf-8'))
 
             resposta = s.recv(1024).decode('utf-8')
-            if resposta != 'AUTENTICACAO OK':
-                print("Falha na autenticação, tente novamente")
+            if not resposta.startswith('AUTENTICACAO OK'):
+                print("\nFalha na autenticação, tente novamente")
                 time.sleep(1)
                 return
-            
-            print('Autenticado com sucesso! Comandos disponíveis: ')
-            time.sleep(1)
-            print('HORA - Solicitar a hora atual')
-            print('DATA - Solicitar data atual')
-            print('@usuario mensagem - Mensagem privada')
 
-            #Thread para receber mensagens
+            print("\n" + resposta)
+            print('\nComandos disponíveis:')
+            print('HORA   - Solicitar a hora atual')
+            print('DATA   - Solicitar a data atual')
+            print('LISTAR - Ver usuários conectados')
+            print('@usuario mensagem - Enviar mensagem privada')
+            print('SAIR   - Encerrar conexão')
+
+            # Thread para receber mensagens
             thread_receber = threading.Thread(target=receber_mensagens, args=(s,))
+            thread_receber.daemon = True
             thread_receber.start()
 
-            #Envio de mensagens
+            # Envio de mensagens
             while True:
                 mensagem = input('Digite sua mensagem: ')
                 if mensagem.lower() == 'sair':
+                    print("Desconectando do servidor...")
                     break
                 s.sendall(mensagem.encode('utf-8'))
+
+        except ConnectionRefusedError:
+            print("Não foi possível conectar ao servidor.")
+        except Exception as e:
+            print(f"Ocorreu um erro: {e}")
+
 if __name__ == '__main__':
     iniciar_cliente()
